739dff414e81e514ca3936251072b4b1b4450939
wushuai <fightingwushuai@qq.com>
2018-11-17 14:52:58 +0800

    第一版将工程添加到本地仓库

---
+++ crawler_webmagic/src/main/java/com/flowercity/CrawlerWebmagicApplication.java
@@ -0,0 +1,12 @@
+package com.flowercity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerWebmagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerWebmagicApplication.class, args);
+    }
+}
